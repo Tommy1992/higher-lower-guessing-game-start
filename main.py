38136ab4424a8a0cb1ada7ef950de5d220749a4f
Tommy1992 <29047092+Tommy1992@users.noreply.github.com>
2022-01-05 13:50:49 +0000

    added newB(...) +x

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ ToDo list:
 1 - CHECK import random, art, game_data   
 2 - CHECK printing logo1, logo2 VS, in between print Compare A: {var} and then Against B: {var2}, then "Who more follows, type..." 
 3 - function to access and compare data(imported already), setting var2 to var1 after a right answer - then wait for input of answer again
-3.1 - compare fct:
+3.1 - compare fct: Add utility, that not the same company/celeb is chosen to compare (or is there no chance with random.choice)
 4 - global variables needed !?(i.e. score)
 """
 
@@ -23,28 +23,49 @@ bfoll = b["follower_count"]
 bdesc = b["description"]
 bcountry = b["country"]
 
+lose = False
+
 print (logo)
 
 
 def game():
-  # print (b)
-  # print (bfoll)
   print (f"Compare A: {aname}, a {adesc}, from {acountry}.{vs}\nAgainst B: {bname}, a {bdesc}, from {bcountry}.\n")
-  #comparing code
-  compare(score)
+  compare(score, lose,a,b,aname, afoll,adesc,acountry,bname,bfoll,bdesc,bcountry)
 
-def compare(score):
-  aORb = input("Who has more followers? Type 'A' or 'B'.") 
-  if (aORb.lower == "a" and afoll >= bfoll) or (aORb.lower == "b" and bfoll >= afoll):
-    score +=1
-    print (f"You are right. Your new score is: {score}.\n{aname} has {afoll} followers and {bname} has {bfoll} followers.")
-    return score
-  else:
-    print(f"Sorry, that's wrong.\n{aname} has {afoll} million followers and {bname} has {bfoll} million followers. Final score: {score}.")  #score return needed now after this line?
+def compare(score, lose,a,b,aname, afoll,adesc,acountry,bname,bfoll,bdesc,bcountry):
+  while lose == False:
+    aORb = input("Who has more followers? Type 'A' or 'B'.") 
+    if (aORb == "a" and afoll >= bfoll) or (aORb == "b" and bfoll >= afoll):
+      score +=1
+      print (f"You are right. Your new score is: {score}.\n{aname} has {afoll} million followers and {bname} has {bfoll} million followers.")
+      aname = b["name"] #line is wanted with b[name]
+      afoll = b["follower_count"]
+      adesc = b["description"]
+      acountry = b["country"]
+      b = rnd.choice(data)
+      bname = b["name"]
+      bfoll = b["follower_count"]
+      bdesc = b["description"]
+      bcountry = b["country"]
+    #newb(a,b, aname, afoll,adesc,acountry,bname,bfoll,bdesc,bcountry)
+    else:
+        lose = True
+        print(f"Sorry, that's wrong.\n{aname} has {afoll} million followers and {bname} has {bfoll} million followers. Final score: {score}.")  #score return needed now after this line?
 
-
+def newB(a,b,aname, afoll,adesc,acountry,bname,bfoll,bdesc,bcountry): #no pun intended
+    aname = b["name"] #line is wanted with b[name]
+    afoll = b["follower_count"]
+    adesc = b["description"]
+    acountry = b["country"]
+    b = rnd.choice(data)
+    bname = b["name"]
+    bfoll = b["follower_count"]
+    bdesc = b["description"]
+    bcountry = b["country"]
+    return (a,b,aname, afoll,adesc,acountry,bname,bfoll,bdesc,bcountry)
 
 game()
+
 
 """ data = [
     {
@@ -53,4 +74,14 @@ game()
         'description': 'Social media platform',
         'country': 'United States'
     },
+
+          aname = b["name"] #line is wanted with b[name]
+      afoll = b["follower_count"]
+      adesc = b["description"]
+      acountry = b["country"]
+      b = rnd.choice(data)
+      bname = b["name"]
+      bfoll = b["follower_count"]
+      bdesc = b["description"]
+    bcountry = b["country"]
 """
