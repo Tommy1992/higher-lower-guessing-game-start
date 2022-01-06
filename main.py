51280a105f215a30a4ee4799bf1d68753597533f
Tommy1992 <29047092+Tommy1992@users.noreply.github.com>
2022-01-06 15:19:10 +0000

    debugging pritn statements - in order to show always the game logo (while also including cls / clear)

--- main.py
+++ main.py
@@ -10,8 +10,13 @@ ToDo list:
 from art import logo, vs
 from game_data import data
 import random as rnd
+import os
+
+def cls():
+    os.system('cls' if os.name=='nt' else 'clear')
 
 score = 0
+
 a = rnd.choice(data)
 aname = a["name"]
 afoll = a["follower_count"]
@@ -29,13 +34,15 @@ print (logo)
 
 
 def game():
-  print (f"Compare A: {aname}, a {adesc}, from {acountry}.{vs}\nAgainst B: {bname}, a {bdesc}, from {bcountry}.\n")
   compare(score, lose,a,b,aname, afoll,adesc,acountry,bname,bfoll,bdesc,bcountry)
 
 def compare(score, lose,a,b,aname, afoll,adesc,acountry,bname,bfoll,bdesc,bcountry):
   while lose == False:
+    print (f"Compare A: {aname}, a {adesc}, from {acountry}.{vs}\nAgainst B: {bname}, a {bdesc}, from {bcountry}.\n")
     aORb = input("Who has more followers? Type 'A' or 'B'.") 
     if (aORb == "a" and afoll >= bfoll) or (aORb == "b" and bfoll >= afoll):
+      cls()
+      print (logo)
       score +=1
       print (f"You are right. Your new score is: {score}.\n{aname} has {afoll} million followers and {bname} has {bfoll} million followers.")
       aname = b["name"] #line is wanted with b[name]
@@ -65,23 +72,3 @@ def newB(a,b,aname, afoll,adesc,acountry,bname,bfoll,bdesc,bcountry): #no pun in
     return (a,b,aname, afoll,adesc,acountry,bname,bfoll,bdesc,bcountry)
 
 game()
-
-
-""" data = [
-    {
-        'name': 'Instagram',
-        'follower_count': 346,
-        'description': 'Social media platform',
-        'country': 'United States'
-    },
-
-          aname = b["name"] #line is wanted with b[name]
-      afoll = b["follower_count"]
-      adesc = b["description"]
-      acountry = b["country"]
-      b = rnd.choice(data)
-      bname = b["name"]
-      bfoll = b["follower_count"]
-      bdesc = b["description"]
-    bcountry = b["country"]
-"""
